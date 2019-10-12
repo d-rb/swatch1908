ee4178c04c768efb983abf771a43ea75e3dfd724
d-rb <1028952640@qq.com>
2019-10-12 11:03:48 +0800

    ssss

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+// const minify_css = require("gulp-minify-css");
+///是gulp所完成的事情（任务）
+
+//定义一个复制文件的任务
+// gulp.task("copy-html", async ()=>{
+//     //把当前目录下的所有html文件拷贝至目录：D:\\phpStudy\\WWW\\web1908\\taobao
+//     gulp.src("*.html")
+//     .pipe(gulp.dest("D:\\phpStudy\\WWW\\web1908\\taobao"));
+// });
+
+// gulp.task("watchall",async ()=>{
+    //定义一个监听器，监听文件是否有改变，如果有改变，则自动拷贝。
+    // gulp.watch("*.html",async ()=>{
+        //把当前目录下的所有html文件拷贝至目录：D:\\phpStudy\\WWW\\web1908\\taobao
+//         gulp.src("*.html")
+//         .pipe(gulp.dest("D:\\phpStudy\\WWW\\web1908\\taobao"));
+//     });
+
+//     gulp.watch("js/*.js",async ()=>{
+//         gulp.src("js/*.js")
+//         .pipe(uglify())
+//         .pipe(gulp.dest("D:\\phpStudy\\WWW\\web1908\\taobao\\js"));
+//     })
+// });
+gulp.task("watchall",async ()=>{
+    gulp.watch("*.html", async ()=>{
+        //把当前目录下的所有html文件拷贝至目录：D:\phpStudy\WWW\myfile\swatch1908
+        gulp.src("*.html")
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\swatch1908"));
+    })
+    gulp.watch("*.scss", async ()=>{
+        gulp.src("*.scss")
+        .pipe(sass())
+        // .pipe(minify_css())
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\swatch1908\\css"));
+    })
+});
---
+++ js/hahaha.js
@@ -0,0 +1,124 @@
+// $(".a1").mouseenter(function(){
+//     $(".t1").slideDown(1000);
+// });
+// $(".a1").mouseleave(function(){
+//     $(".t1").css("display","none");
+// });
+// $(".a2").mouseenter(function(){
+//     $(".t2").slideDown(1000);
+// });
+// $(".a2").mouseleave(function(){
+//     $(".t2").css("display","none");
+// });
+// $(".a3").mouseenter(function(){
+//     $(".t3").slideDown(1000);
+// });
+// $(".a3").mouseleave(function(){
+//     $(".t3").css("display","none");
+// });
+// $(".a4").mouseenter(function(){
+//     $(".t4").slideDown(1000);
+// });
+// $(".a4").mouseleave(function(){
+//     $(".t4").css("display","none");
+// });
+
+let myTimer=0;
+$(".a1").mouseenter(function(){
+	if($(".t1").css("display","block")){
+		console.log("checkclass:mouseenter");
+		$(".t1").slideDown();
+	}
+	
+});
+
+$(".a1").mouseleave(function(){
+    myTimer=setTimeout(function(){
+		$(".t1").slideUp();
+		console.log(11111);
+	},200);
+});
+
+$(".t1").mouseenter(function(){	
+	clearInterval(myTimer);
+	//$(this).show();	
+});
+
+$(".t1").mouseleave(function(){
+	$(this).hide();	
+})
+
+
+$(".a2").mouseenter(function(){
+	if($(".t2").css("display","block")){
+		console.log("checkclass:mouseenter");
+		$(".t2").slideDown();
+	}
+	
+});
+
+$(".a2").mouseleave(function(){
+    myTimer=setTimeout(function(){
+		$(".t2").slideUp();
+		console.log(11111);
+	},200);
+});
+
+$(".t2").mouseenter(function(){	
+	clearInterval(myTimer);
+	//$(this).show();	
+});
+
+$(".t2").mouseleave(function(){
+	$(this).hide();	
+})
+
+
+
+$(".a3").mouseenter(function(){
+	if($(".t3").css("display","block")){
+		console.log("checkclass:mouseenter");
+		$(".t3").slideDown();
+	}
+	
+});
+
+$(".a3").mouseleave(function(){
+    myTimer=setTimeout(function(){
+		$(".t3").slideUp();
+		console.log(11111);
+	},200);
+});
+
+$(".t3").mouseenter(function(){	
+	clearInterval(myTimer);
+	//$(this).show();	
+});
+
+$(".t3").mouseleave(function(){
+	$(this).hide();	
+})
+
+$(".a4").mouseenter(function(){
+	if($(".t4").css("display","block")){
+		console.log("checkclass:mouseenter");
+		$(".t4").slideDown();
+	}
+	
+});
+
+$(".a4").mouseleave(function(){
+    myTimer=setTimeout(function(){
+		$(".t4").slideUp();
+		console.log(11111);
+	},200);
+});
+
+$(".t4").mouseenter(function(){	
+	clearInterval(myTimer);
+	//$(this).show();	
+});
+
+$(".t4").mouseleave(function(){
+	$(this).hide();	
+})
